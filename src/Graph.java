1d1440fa47c69d85eb77f4385bbbb52ed2a1fced
Alex Kinnear <alexander.kinnear@aggiemail.usu.edu>
2020-04-18 06:57:45 -0600

    files updated

--- src/Graph.java
+++ src/Graph.java
@@ -1,124 +1,131 @@
 import java.io.File;
-import java.util.ArrayList;
-import java.util.Scanner;
-import java.util.Arrays;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+
+class Node {
+    ArrayList<Edge> edges = new ArrayList<>();
+}
+
+class Edge {
+
+    int start;
+    int end;
+    int flow;
+    int capacity;
+
+
+    public Edge(int start, int end, int flow, int capacity) {
+        this.start = start;
+        this.end = end;
+        this.flow = flow;
+        this.capacity = capacity;
+    }
+
+}
+
 
 public class Graph {
-    int vertexCt;  // Number of vertices in the graph.
-    int edgeCount;
-    GraphNode[] G;  // Adjacency list for graph.
-    Integer[][] capacity;
 
-    String graphName;  //The file from which the graph was created.
+    public static void main(String[] args) throws FileNotFoundException {
+        String filename = "demands1.txt";
+        ArrayList<Edge> edgeList = new ArrayList<>();
+        System.out.println(filename);
+        Scanner scan = new Scanner(new File("data/" + filename));
 
-    public Graph() {
-        this.vertexCt = 0;
-        this.edgeCount = 0;
-        this.graphName = "";
-    }
+        int nodes = scan.nextInt();
+        int source = 0;
+        int sink = nodes - 1;
 
-    public static void main(String[] args) {
-        Graph graph1 = new Graph();
-        graph1.makeGraph("demands1.txt");
-//        System.out.println(graph1.toString());
-        System.out.println(graph1.maxFlow(0, 5));
+        Node[] graph = new Node[nodes];
+
+        // initialize nodes
+        for (int i = 0; i < nodes; i++)
+            graph[i] = new Node();
+
+        // read in edges from file
+        while (scan.hasNextInt()) {
+            int start = scan.nextInt();
+            int end = scan.nextInt();
+            int capacity = scan.nextInt();
+
+            Edge a = new Edge(start , end , 0 , capacity);
+            Edge b = new Edge(end , start , 0 , 0);
 
 
-    }
-
-    public int getVertexCt() {
-        return vertexCt;
-    }
-
-    public boolean addEdge(int source, int destination, int cap) {
-        //System.out.println("addEdge " + source + "->" + destination + "(" + cap + ")");
-        if (source < 0 || source >= vertexCt) return false;
-        if (destination < 0 || destination >= vertexCt) return false;
-
-        //add edge
-        G[source].addEdge(source, destination, cap);
-        capacity[source][destination] = cap;
-
-        edgeCount++;
-        return true;
-    }
-
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("The Graph " + graphName + " \n");
-
-        for (int i = 0; i < vertexCt; i++) {
-            sb.append(G[i].toString());
+            graph[start].edges.add(a);
+            graph[end].edges.add(b);
         }
-        return sb.toString();
-    }
+        scan.close();
 
-    public void makeGraph(String filename) {
-
-        try {
-            graphName = filename;
-            Scanner reader = new Scanner(new File("data/"+filename));
-            vertexCt = reader.nextInt();
-
-            // initialize the arrays of length vertexCt
-            G = new GraphNode[vertexCt];
-            capacity = new Integer[vertexCt][vertexCt];
+        // sum of flow for all paths
+        int totalFlow = 0;
 
 
+        System.out.println("MAX FLOW:");
+        while (true) {
 
+            Edge[] parent = new Edge[nodes];
 
-            // fill arrays
-            for (int i = 0; i < vertexCt; i++) {
-                G[i] = new GraphNode(i);
-                for (int j = 0; j < vertexCt; j++) {
-                    capacity[i][j] = 0;
-                }
-            }
+            ArrayList<Node> q = new ArrayList<>();
+            q.add(graph[source]);
 
-            while (reader.hasNextInt()) {
-                int v1 = reader.nextInt();
-                int v2 = reader.nextInt();
-                int cap = reader.nextInt();
-                if (!addEdge(v1, v2, cap))
-                    throw new Exception();
-            }
-            reader.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+            // breadth first search
+            while (!q.isEmpty()) {
+                Node current = q.remove(0);
 
-    public int maxFlow( int start, int end) {
-        int flow = Integer.MAX_VALUE;
-        GraphNode.EdgeInfo[] parent = new GraphNode.EdgeInfo[vertexCt];
-        // breadth first search
-        do {
-            MyQueue<Integer> nodes = new MyQueue<>();
-            nodes.enqueue(start);
-
-            while (!nodes.isEmpty()) {
-                int current = nodes.dequeue();
-                for (GraphNode.EdgeInfo edge : G[current].succ) {
-                    if (parent[edge.to] == null && edge.to != start && edge.capacity > 0) {
-                        parent[edge.to] = edge;
-                        nodes.enqueue(edge.to);
+                for (Edge edge : current.edges)
+                    if (parent[edge.end] == null && edge.end != source && edge.capacity > edge.flow) {
+                        parent[edge.end] = edge;
+                        q.add(graph[edge.end]);
                     }
+            }
+
+            // If no path were found (min cut)
+            if (parent[sink] == null)
+                break;
+
+            // flow for each path
+            int flow = 1000;
+
+            for (Edge edge = parent[sink]; edge != null; edge = parent[edge.start]) {
+                flow = Math.min(flow, edge.capacity - edge.flow);
+            }
+
+            ArrayList<Integer> path = new ArrayList<>();
+            // Update flow value for current path
+            for (Edge edge = parent[sink]; edge != null; edge = parent[edge.start]) {
+                path.add(edge.end);
+
+                if (!edgeList.contains(edge)) {
+                    edgeList.add(edge);
                 }
+
+                edge.flow += flow;
+
             }
+            path.add(0);
 
-
-            int temp = capacity[parent[end].from][parent[end].to];
-            for (int i = end; i != start; i = parent[i].from)
-                temp = Math.min(temp, (capacity[parent[i].from][i]));
-
-            for (int i = end; i != start; i = parent[i].from) {
-                flow += temp;
+            // print out the path and flow from each path
+            System.out.print("Found flow " + flow + ": ");
+            for (int i = path.size() - 1; i >= 0; i--) {
+                System.out.print(path.get(i) + " ");
             }
-            end = parent[end].from;
+            System.out.println();
 
+            totalFlow += flow;
         }
-        while (parent[end] != null);
 
-        return flow;
+        // print out total flow of graph
+        System.out.println("Produced " + totalFlow);
+
+
+        System.out.println();
+        for (Edge edge : edgeList) {
+            System.out.print("Edge (" + edge.start + ", " + edge.end + ") transports " + edge.flow + " cases" + "\n");
+        }
+
+
     }
 }
+
